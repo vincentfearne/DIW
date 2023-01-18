86dbde92703dbce25f0bda8bcdc11d69003b4e70
Fearne <vincent.fearne@gmail.com>
2023-01-18 14:36:44 +0100

    record

---
+++ BACK/SERVEUR/PHP/discs.php
@@ -0,0 +1,46 @@
+<?php
+
+    // on importe le contenu du fichier "db.php"
+    include "db.php";
+    // on exécute la méthode de connexion à notre BDD
+    $db = connexionBase();
+
+    // on lance une requête pour chercher toutes les fiches d'artistes
+    $requete = $db->query("SELECT * FROM disc");
+    // on récupère tous les résultats trouvés dans une variable
+    $tableau = $requete->fetchAll(PDO::FETCH_OBJ);
+    // on clôt la requête en BDD
+    $requete->closeCursor();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PDO - Liste</title>
+</head>
+<body>
+<table>
+        <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <!-- Ici, on ajoute une colonne pour insérer un lien -->
+            <th></th>
+        </tr>
+
+        <?php foreach ($tableau as $disc): ?>
+        <tr>
+            <td><?= $disc->disc_id ?></td>
+            <td><?= $disc->disc_title ?></td>
+            <!-- Ici, on ajoute un lien par artiste pour accéder à sa fiche : -->
+            <td><a href="artist_detail.php?id=<?= $artist->artist_id ?>">Détail</a></td>
+        </tr>
+        <?php endforeach; ?>
+
+    </table>
+</body>
+</html>
